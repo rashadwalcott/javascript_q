5e8b2d503309819a72991089208f98c87ddeea14
Rashad <rashad.walcott15@gmail.com>
2020-01-30 16:42:08 -0500

    working on components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import './App.css';
-import Questions from './quizQuestions';
 import { quizzes } from './quizzes';
 import { getMessage} from './messages';
 import React, { Component } from 'react'
 import MultipleChoiceQuiz from './MultipleChoiceQuiz'
 
 export default class App extends Component {
-  // Initializing State
-      // state = {
-      //   quiz: quizzes[0]
-      // }
-      
-
-      // questionList = () => {
-
-      //   let questions = [];
-      //   for (let eachQuest in this.state.quiz.questions){
-      //     questions.push( this.state.quiz.questions[eachQuest])
-      //   }
-      //   return questions;
-      //   }
-      
+  
   render() {
-    // const questionsList = this.state.quiz.questions.map(quest => {
-    //   return <Questions questions = {quest} 
-    //     key = {quest.text}/>
-    // })
-    // console.log(this.state.quiz.questions);
-    
     return (
       <div>
         <MultipleChoiceQuiz quizzes={quizzes} messages = {getMessage} />
-        {/* <h1>{this.state.quiz.title}</h1> */}
-        {/* <Questions questions = {this.questionList()} /> */}
       </div>
     );
   }
--- src/MultipleChoiceQuiz.js
+++ src/MultipleChoiceQuiz.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class MultipleChoiceQuiz extends Component {
     // Initializing State
@@ -7,6 +7,32 @@ export default class MultipleChoiceQuiz extends Component {
         question: 0,
         incomplete: true
     }
+
+     //Function that handles all the answers
+     handleAnswers = () => {
+        let presentQuestion = this.props.quizzes[this.state.quiz].questions[this.state.question];
+        let correctAnswer = presentQuestion.correctAnswer;
+        let incorrectAnswers = presentQuestion.incorrectAnswers;
+        let allAnswers = incorrectAnswers.concat(correctAnswer);
+        let shuffle = require('shuffle-array');
+        return shuffle(allAnswers);
+    }
+    //Function that is mapping and displaying the answers
+    showAnswers = () => {
+        return (
+            <div>
+                <ol type= "A">
+                    {this.handleAnswers().map((answer, index) => (
+                        <li key = {index}>
+                        <span>{answer}</span>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
+
+    //Function that handles the next question
         handleNext = () => {
             if (this.state.question < this.props.quizzes[this.state.quiz].questions.length - 1){
                 this.setState({question: this.state.question + 1})
@@ -15,23 +41,32 @@ export default class MultipleChoiceQuiz extends Component {
                 this.setState({incomplete: false})
             }
         }
-
-        handleAnswers = () => {
-            let presentQuestion = this.props.quizzes[this.state.quiz].questions[this.state.question];
-            let correctAnswer = presentQuestion.correctAnswer;
-            let incorrectAnswers = presentQuestion.incorrectAnswers;
-            let allAnswers = correctAnswer.concat(incorrectAnswers);
-            
-        }
+       
     render() {
-        console.log(this.props.quizzes[this.state.quiz].questions[this.state.quiz]);
+        // console.log(this.handleAnswers());
+        
+        // console.log(this.props.quizzes[this.state.quiz].questions[this.state.quiz]);
+        
         let title = this.props.quizzes[this.state.quiz].title;
-        let question = this.props.quizzes[this.state.quiz].questions
+        let question = this.props.quizzes[this.state.quiz].questions[this.state.question].text;
         return (
             <div>
+                {this.state.incomplete ? 
+                <>
                 <h1>{title}</h1>
-                    
+                <h2>{question}</h2>
+                <div>{this.showAnswers()}</div>
+                <button onClick = {this.handleNext}>
+                    Next
+                </button>
+                </>
+                :
+                <>
+                <h1>Message</h1>
+                </>
+                }
                 
+
             </div>
         )
     }
