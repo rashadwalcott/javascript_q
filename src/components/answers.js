f9bd8e459ebf972cc858c6f6554ede7a495cc92f
Rashad <rashad.walcott15@gmail.com>
2020-01-30 20:52:13 -0500

    making progress on logic

--- src/components/answers.js
+++ src/components/answers.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Answers extends Component {
+    state = {
+        clicked: false,
+        correct: 0,
+        incorrect: 0,
+        id: ''
+    }
     //Function that is mapping and displaying the answers
     showAnswers = () => {
         return (
             <div>
                 <ol type= "A">
                     {this.props.answers.map((answer, index) => (
-                        <li key = {index}>
-                        <span>{answer}</span>
+                        <li key = {index}
+                        onClick= {event => this.checkAnswer(event,index)}>
+                        {answer}
                         </li>
                     ))}
                 </ol>
@@ -16,7 +23,13 @@ export default class Answers extends Component {
         )
     }
 
-    
+    //Function that is checking for the correct answer
+    checkAnswer = (event, index) =>{
+        console.log(event.target.innerText);
+        
+        
+    }
+
     render() {
         return (
             <div>
